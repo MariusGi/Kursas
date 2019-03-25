cf4d46de2f3faa827cf470df7178ccc6a93261be
MariusGi <mariusgirnis@gmail.com>
2019-03-25 21:06:16 +0200

    PHP2 ND

---
+++ NamuDarbai/PHP/PHP2/index02.php
@@ -0,0 +1,22 @@
+<?php
+    $country = 'Lietuva';
+    $city = 'Vilnius';
+    $population = 574221;
+    $largest_population = "true";
+?>
+
+<!DOCTYPE html>
+<html lang="lt">
+    
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP2 ND</title>
+</head>
+
+<body>
+    <p><?php echo $city. ' Lietuvos sostinė'; ?></p>
+    <p><?php echo "$city ir $country sparčiai auga" ?></p>
+</body>
+    
+</html
