1f8969761321f2ed5746111350ca65a46f51347f
MariusGi <mariusgirnis@gmail.com>
2019-03-28 20:15:39 +0200

    PHP10 ND

---
+++ NamuDarbai/PHP/PHP10/index.php
@@ -0,0 +1,22 @@
+<?php
+    function skaiciuotiPlota($x, $y) {
+        echo $x * $y;
+    }
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP10 ND</title>
+</head>
+
+<body>
+    <?php
+        skaiciuotiPlota(6,7);
+    ?>
+</body>
+    
+</html>
